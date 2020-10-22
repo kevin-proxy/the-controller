5a89172165e2847c5137620e79c9bd7db52c3675
Kevin Cheung <pxrkkmc312@gmail.com>
2020-10-22 23:14:11 +0100

    update

--- index.js
+++ index.js
@@ -232,34 +232,19 @@ client.on('message', message=>{
         }
     }else if (command === 'user-info'){
 
-        /*var userBoolean = message.mentions.users.first().bot
-        var authorBoolean = message.author.bot
-
-        if(userBoolean == true){
-            message.content = 'Bot'
-        }
-        if(userBoolean == false){
-            message.content = 'Human'
-        }
-        if(userBoolean == false){
-            message.content = 'Human'
-        }
-        if(userBoolean == true){
-            message.content = 'Bot'
-        }*/
-
         if(!message.mentions.users.size){
             
         const memberAuthorInfo = message.guild.member(message.author)
+
+        const adminRole = message.guild.role('714927110682050590')
             
         const myInfoEmbed = new Discord.MessageEmbed();
         myInfoEmbed.setTitle(message.author.username)
-        myInfoEmbed.setDescription(`**Tag:** ${message.author.tag}\n**ID:** ${message.author.id}`)
+        myInfoEmbed.setDescription(`**Tag:** ${message.author.tag}\n**ID:** ${message.author.id}\n**Bot Account:** ${message.author.bot}\n`)
         myInfoEmbed.setColor(0x3366ff)
         myInfoEmbed.addFields(
             {name: 'Account Created At', value: `${message.author.createdAt}`, inline: true},
             {name: 'Joined Server At', value: `${memberAuthorInfo.joinedAt}`, inline: true},
-            {name: 'Bot Account', value: message.author.bot, inline: true}
             )
         myInfoEmbed.setFooter(`Requested by ${message.author.username}`)
         myInfoEmbed.setThumbnail(`${message.author.displayAvatarURL({dynamic: true})}`)
@@ -272,13 +257,12 @@ client.on('message', message=>{
             
             const userInfoEmbed = new Discord.MessageEmbed();
             userInfoEmbed.setTitle(message.mentions.users.first().username)
-            userInfoEmbed.setDescription(`**Tag:** ${message.mentions.users.first().tag}\n**ID:** ${message.mentions.users.first().id}`)
+            userInfoEmbed.setDescription(`**Tag:** ${message.mentions.users.first().tag}\n**ID:** ${message.mentions.users.first().id}\n**Bot Account:** ${message.mentions.users.first().bot}`)
             userInfoEmbed.setColor(0x3366ff)
             userInfoEmbed.setThumbnail(`${message.mentions.users.first().displayAvatarURL({dynamic: true})}`)
             userInfoEmbed.addFields(
                 {name: 'Account Created At', value: `${message.mentions.users.first().createdAt}`, inline: true},
                 {name: 'Joined Server At', value: `${memberInfo.joinedAt}`, inline: true},
-                {name: 'Bot Account', value: message.mentions.users.first().bot, inline: true}
                 )
             userInfoEmbed.setFooter(`Requested by ${message.author.username}`)
             
