5e02a2e14c57db3690f715c02d73eb5fea352027
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 11:31:15 +0100

    Update and rename prefix.js to commands/prefix.js

---
+++ commands/prefix.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'prefix',
+  description: 'Displays the prefix of this bot',
+  execute(message,args){
+    if (message.channel.type == "dm") return;
+        
+        const prefixEmbed = new Discord.MessageEmbed();
+        prefixEmbed.setTitle('Prefix')
+        prefixEmbed.setDescription("This bot's prefix is `" + `${prefix}` + "`")
+        prefixEmbed.setColor(0x3366ff)
+        
+        message.channel.send(prefixEmbed);
+  },
+};
