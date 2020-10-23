4a93bca9cfcbd5a611d467a5aee8cb11934f70d1
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 18:10:43 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'ban',
   description: 'Ban members',
-  execute(message, args, Discord, client){
+  execute(client, message, args, Discord){
     
     const { MessageEmbed } = module.require('discord.js');
     
