454de9d738fb79febf07d712b404cb2456cdde2a
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 11:58:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ client.on('message', message=>{
     
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
+	
+});
 client.login(process.env.BOT_TOKEN);
