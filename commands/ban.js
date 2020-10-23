71499711ca54d5f95544df1bad79f614c3c6899f
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 13:38:12 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'ban',
   description: 'Ban members',
-  execute(message, args, Discord, discord.js){
+  execute(message, args, Discord, MessageEmbed){
     if (message.channel.type == "dm") return;
         
         const userBan = message.mentions.users.first();
