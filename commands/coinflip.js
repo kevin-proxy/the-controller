f6daa2f84822ec900ecb907c4a4057b8bb1bee84
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 11:55:43 +0100

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  name: 'coinflip',
+  description: 'Flips a coin and tells you the result',
+  execute(message, args){
+    function doRandHT(){
+            var rand = ['Heads!','Tails!'];
+            return rand[Math.floor(Math.random()*rand.length)];
+        }
+            
+        const htEmbed = new Discord.MessageEmbed();
+        htEmbed.setTitle('Coinflip results!')
+        htEmbed.setDescription(doRandHT())
+        htEmbed.setColor(0x3366ff)
+        
+        message.channel.send(htEmbed)
+  },
+};
