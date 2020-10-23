3e6f7761698b4a4e514f6d8a430e40b21807da8a
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 10:56:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,54 +26,7 @@ client.on('message', message=>{
     if (!client.commands.has(command)) return;
 
     if(command === 'purge'){
-        
-        if (message.channel.type == "dm") return;
-           
-        }else{
-            if(!message.member.hasPermission('MANAGE_MESSAGES')){
-                
-            const purgePermissionEmbed = new Discord.MessageEmbed();
-            purgePermissionEmbed.setDescription('You do not have permission to purge messages!')
-            purgePermissionEmbed.setColor(0x3366ff)
-                
-            return message.reply(purgePermissionEmbed);
-                
-            }else if(isNaN(args[0])){
-                
-            const purgeNaNEmbed = new Discord.MessageEmbed();
-            purgeNaNEmbed.setDescription('Please provide a number of messages to purge!')
-            purgeNaNEmbed.setColor(0x3366ff)
-                
-            return message.reply(purgeNaNEmbed).then(msg => msg.delete({timeout: 3000}));
-                
-            }else if(args[0] < 2 || args[0] > 99){
-                
-            const purgeBetweenEmbed = new Discord.MessageEmbed();
-            purgeBetweenEmbed.setDescription('Please provide a number between or equal to 2 and 99!')
-            purgeBetweenEmbed.setColor(0x3366ff)
-                
-            return message.reply(purgeBetweenEmbed).then(msg => msg.delete({timeout: 3000}));
-                
-            }
-            
-            const purgeErrEmbed = new Discord.MessageEmbed();
-            purgeErrEmbed.setDescription('An error occured, please try again later...')
-            purgeErrEmbed.setColor(0x3366ff)
-            
-            message.channel.bulkDelete(parseInt(args[0]) + 1, true).catch(err =>{
-            console.error(err);
-            message.channel.send(purgeErrEmbed).then(msg => msg.delete({timeout: 3000}));
-            }).then(()=>{
-            
-            const purgeClearedEmbed = new Discord.MessageEmbed();
-            purgeClearedEmbed.setDescription(`Successfully purged ${args[0]} messages!`)
-            purgeClearedEmbed.setColor(0x3366ff)
-            
-            message.channel.send(purgeClearedEmbed).then(msg => msg.delete({timeout: 3000}));
-            }).catch(err => {
-            console.error(err);
-            })
-        };  
+	    client.commands.get('purge').execute(message,args);
     }else if(command === 'kick'){
         
         if (message.channel.type == "dm") return;
