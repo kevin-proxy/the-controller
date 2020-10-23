731ec0f4bbca358c1a98e2b037c8c432c105c137
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 10:37:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+
 const client = new Discord.Client();
-const prefix = '>';
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () =>{
     console.log('Ready');
     client.user.setActivity('>commands for info')
 })
 
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 client.on('message', message=>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
 
     if(command === 'purge'){
         
