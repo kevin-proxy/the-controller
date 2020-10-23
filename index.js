42a7c0b5a6f8f441a07a1ced4927635e638216b8
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 11:11:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,55 +32,7 @@ client.on('message', message=>{
         
     }else if(command === 'ban'){
         
-        if (message.channel.type == "dm") return;
         
-        const userBan = message.mentions.users.first();
-        
-        const reasonBan = args.slice(100).join(` `);
-        
-        const banPermissionEmbed = new Discord.MessageEmbed();
-        banPermissionEmbed.setDescription('You do not have permission to ban members!')
-        banPermissionEmbed.setColor(0x3366ff)
-        
-        const banLimitEmbed = new Discord.MessageEmbed();
-        banLimitEmbed.setDescription('You have exceeded the 100 word limit for your ban reason')
-        banLimitEmbed.setColor(0x3366ff)
-        
-        const banSuccessEmbed = new Discord.MessageEmbed();
-        banSuccessEmbed.setDescription(`Successfully banned ${userBan}`)
-        banSuccessEmbed.setColor(0x3366ff)
-        
-        const banErrEmbed = new Discord.MessageEmbed();
-        banErrEmbed.setDescription('An error occured, please try again later...')
-        banErrEmbed.setColor(0x3366ff)
-        
-        const banArgsEmbed = new Discord.MessageEmbed();
-        banArgsEmbed.setDescription('Please mention a user to ban!')
-        banArgsEmbed.setColor(0x3366ff)
-        
-        if(!message.member.hasPermission('BAN_MEMBERS')){
-           return message.channel.send(banPermissionEmbed).then(msg => msg.delete({timeout: 3000}));
-        };
-        
-        if(args[101]){
-            return message.channel.send(banLimitEmbed).then(msg => msg.delete({timeout: 3000}));
-        };
-        
-        if(userBan){
-            
-            const memberBan = message.guild.member(userBan);
-
-            if(memberBan){
-                memberBan.ban({reason: reasonBan}).then(() =>{
-                    message.channel.send(banSuccessEmbed);
-                }).catch(err =>{
-                    message.channel.send(banErrEmbed).then(msg => msg.delete({timeout: 3000}));
-                    console.error(err);
-                });
-            }
-        }else{
-            return message.reply(banArgsEmbed)
-        }
     }else if(command === 'commands'){
         if (message.channel.type == "dm") return;
 
