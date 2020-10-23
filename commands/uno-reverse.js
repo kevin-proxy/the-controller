95aa35c5934ceeeeed0faffac4374df1cc24097e
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-23 11:50:49 +0100

    Update uno-reverse.js

--- commands/uno-reverse.js
+++ commands/uno-reverse.js
@@ -1,1 +1,12 @@
-
+module.exports = {
+  name: 'uno-reverse',
+  description: 'Lol get uno reversed',
+  execute(message, args){
+    if (message.channel.type == "dm") return;
+        if(message.mentions.users.first()){
+            message.channel.send(`Haha ${message.mentions.users.first()}, get uno reversed`,{files:["https://i.imgur.com/WUX7tbB.png"]})
+        }else{
+            message.channel.send('Dumbass, you just uno reversed yourself',{files:["https://i.imgur.com/WUX7tbB.png"]})
+        }
+  },
+};
