f3e616dfcb37d44477bdd6a340fedf18b38cc530
akaproxygithub <71177806+akaproxygithub@users.noreply.github.com>
2020-10-24 01:52:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+module.exports = (Discord, prefix);
+
 client.once('ready', () =>{
     console.log('Ready');
     client.user.setActivity('DM me for help! >help')
