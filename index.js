b4df8b04877a037f99b8acbf4f956f3465a54e40
Kevin Cheung <pxrkkmc312@gmail.com>
2020-11-06 20:24:24 +0000

    rewrite part 3 (big meaty section)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//bot configuration
+/*//bot configuration
 const Discord = require("discord.js");
 const client = new Discord.Client();
 global.client = client;
@@ -120,4 +120,68 @@ client.on("guildMemberAdd", (member) => {
 });
 
 //log in
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);*/
+
+require('module-alias/register')
+require("dotenv").config()
+
+// const Discord = require('discord.js')
+// const client = new Discord.Client()
+
+const { MongoClient } = require('mongodb')
+//const MongoDBProvider = require('commando-provider-mongo')
+const path = require('path')
+const Commando = require('discord.js-commando')
+
+//const config = require('@root/config.json')
+const loadCommands = require('@root/commands/load-commands')
+const commandBase = require('@root/commands/command-base')
+const loadFeatures = require('@root/features/load-features')
+const mongo = require('@util/mongo')
+
+const modLogs = require('@features/mod-logs')
+
+const client = new Commando.CommandoClient({
+  owner: '381906626581626880',
+  commandPrefix: process.env.PREFIX,
+})
+
+client.setProvider(
+  MongoClient.connect(process.env.MONGO_PATH, {
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+    .then((client) => {
+      return new MongoDBProvider(client, 'WornOffKeys')
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+)
+
+client.on('ready', async () => {
+  console.log('The client is ready!')
+
+  await mongo()
+
+  client.registry
+    .registerGroups([
+      ['misc', 'misc commands'],
+      ['moderation', 'moderation commands'],
+      ['economy', 'Commands for the economy system'],
+      ['giveaway', 'Commands to manage giveaways'],
+      ['games', 'Commands to handle games'],
+      ['thanks', 'Commands to help thank people'],
+      ['suggestions', 'Commands regarding suggestions'],
+      ['testing', 'Commands to test joining and leaving'],
+    ])
+    .registerDefaults()
+    .registerCommandsIn(path.join(__dirname, 'cmds'))
+  // commandBase.loadPrefixes(client)
+  // loadCommands(client)
+  loadFeatures(client)
+
+  modLogs(client)
+})
+
+client.login(process.env.BOT_TOKEN)
