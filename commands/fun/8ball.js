632d63a0a71b070d1700b8616832e4fbbc9dee2b
Kevin Cheung <pxrkkmc312@gmail.com>
2020-11-26 21:15:12 +0000

    took away message.delete()

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -14,7 +14,6 @@ module.exports = class eightBallCommand extends (
   }
 
   async run(message, args) {
-    message.delete()
     if (!args[1]) return message.reply("Give me something to predict silly");
     function eightBall() {
       var replies = [
